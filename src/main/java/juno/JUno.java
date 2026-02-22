0d767e18bde1c0ef087446d754044a2248d06680
Daniel Miretsky <miretskyd@wit.edu>
2026-02-22 17:28:39 -0500

    Make game a class variable in JUno

--- src/main/java/juno/JUno.java
+++ src/main/java/juno/JUno.java
@@ -8,6 +8,7 @@ import java.lang.NumberFormatException;
 public class JUno {
     public static ArrayList<Player> players = new ArrayList<Player>();
     public static Deck deck;
+    public static Game game;
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -53,7 +54,7 @@ public class JUno {
             players.add(player);
         }
 
-        Game game = new Game(players);
+        game = new Game(players);
         deck = new Deck(game);
         game.startGame(players);
 
